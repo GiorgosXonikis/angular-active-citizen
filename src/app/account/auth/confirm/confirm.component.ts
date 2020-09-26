12ec51eeb3f4858f5f54bcf229acd44f78547afe
gxo <georgios.xonikis@gmail.com>
2020-09-26 13:24:47 +0200

    refactored login

--- src/app/account/auth/confirm/confirm.component.ts
+++ src/app/account/auth/confirm/confirm.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-confirm',
-  templateUrl: './confirm.component.html',
-  styleUrls: ['./confirm.component.scss']
+    selector: 'app-confirm',
+    templateUrl: './confirm.component.html',
+    styleUrls: ['./confirm.component.scss']
 })
 export class ConfirmComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,68 +2,86 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {CookieService} from './cookie.service';
 import {IUser} from '../models/interfaces';
-import {Observable} from "rxjs";
+import {tap} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
-  public user: IUser;
-  private _token: string;
+    public user: IUser;
+    private _token: string;
 
-  constructor(private http: HttpClient,
-              private cookieService: CookieService) {
-  }
-
-  public getLoggedInUser(): IUser {
-    if (!this.user) {
-      this.user = JSON.parse(this.cookieService.getCookie('loggedInUser'));
+    constructor(private http: HttpClient,
+                private cookieService: CookieService) {
     }
-    return this.user;
-  }
 
-  public get token(): string {
-    if (!this._token) {
-      this.user = JSON.parse(this.cookieService.getCookie('loggedInUser'));
-      return this.user.token;
+    /**
+     * Performs the auth
+     * @param username of user
+     * @param password password of user
+     */
+    public login(username: string, password: string): Observable<any> {
+        const URL = 'http://localhost:8000/api/token/';
+        return this.http.post<any>(URL, {username, password}).pipe(
+            tap(response => this.getUserProfile(response.access)
+                .subscribe(
+                    userProfile => this.extractUserData(userProfile)
+                ))
+        );
     }
-    return this._token;
-  }
 
-  public set token(value: string) {
-    this._token = value;
-  }
+    public getUserProfile(token: string): Observable<any> {
+        const URL = 'http://localhost:8000/api/me/';
+        const headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+        });
+        return this.http.get(URL, {headers});
+    }
 
-  public getUserProfile(token: string): Observable<any> {
-    const URL = 'http://localhost:8000/api/me/';
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`
-    });
-    return this.http.get(URL, { headers });
-  }
+    private extractUserData(user): void {
+        this.user = {};
+        this.user.id = user['id'];
+        this.user.username = user['username'];
+        this.user.email = user['email'];
+        this.user.firstName = user['first_name'];
+        this.user.lastName = user['last_name'];
+        this.user.avatar = user['avatar'];
+        this.user.location = user['location'];
+        this.user.token = this.token;
+        this.user.firstName;
+        console.log(this.user);
+    }
 
-  /**
-   * Performs the auth
-   * @param username of user
-   * @param password password of user
-   */
-  public login(username: string, password: string): Observable<any> {
-    const URL = 'http://localhost:8000/api/token/';
-    console.log(username);
-    console.log(password);
-    return this.http.post<any>(URL, {username, password});
-  }
+    public getLoggedInUser(): IUser {
+        if (!this.user) {
+            this.user = JSON.parse(this.cookieService.getCookie('loggedInUser'));
+        }
+        return this.user;
+    }
 
-  /**
-   * Logout the user
-   */
-  public logout(): void {
-    // remove user from local storage to log user out
-    this.cookieService.deleteCookie('loggedInUser');
-    this.user = null;
-    this._token = null;
-  }
+    public get token(): string {
+        if (!this._token) {
+            this.user = JSON.parse(this.cookieService.getCookie('loggedInUser'));
+            return this.user.token;
+        }
+        return this._token;
+    }
+
+    public set token(value: string) {
+        this._token = value;
+    }
+
+    /**
+     * Logout the user
+     */
+    public logout(): void {
+        // remove user from local storage to log user out
+        this.cookieService.deleteCookie('loggedInUser');
+        this.user = null;
+        this._token = null;
+    }
 
 
 }
