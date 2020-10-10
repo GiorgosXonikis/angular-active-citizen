bfb6a762d38d9c2f069ba74940c035e993da17dd
gxo <georgios.xonikis@gmail.com>
2020-10-10 15:41:03 +0200

    gitlab ci yml

--- gulpfile.js
+++ gulpfile.js
@@ -6,55 +6,55 @@ const log = require('fancy-log');
 const exec = require('child_process').exec;
 
 const paths = {
-  prod_build: 'production_build',
-  angular_src: 'src',
-  angular_dist: 'dist/frontend',
-  zipped_file_name: 'productionBuild.zip'
+    prod_build: 'production_build',
+    angular_src: 'src',
+    angular_dist: 'dist/frontend',
+    zipped_file_name: 'productionBuild.zip'
 };
 
 function clean() {
-  log('removing the old files in the directory');
-  del('production_build', {force: true});
-  return del('dist', {force: true});
+    log('removing the old files in the directory');
+    del('production_build', {force: true});
+    return del('dist', {force: true});
 }
 
 function createProdBuildDirectory() {
-  const dir = paths.prod_build;
-  log(`Creating the directory if not exist  ${dir}`);
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
-    log('📁  folder created:', dir);
-  }
+    const dir = paths.prod_build;
+    log(`Creating the directory if not exist  ${dir}`);
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+        log('📁  folder created:', dir);
+    }
 
-  return Promise.resolve('the value is ignored');
+    return Promise.resolve('the value is ignored');
 }
 
 function buildAngular(cb) {
-  log('building Angular code into the directory');
-  return exec('ng build', function (err, stdout, stderr) {
-    log(stdout);
-    log(stderr);
-    cb(err);
-  })
+    log('building Angular code into the directory');
+    return exec('ng build', function (err, stdout, stderr) {
+        log(stdout);
+        log(stderr);
+        cb(err);
+    })
 }
 
 function copyAngularBuild() {
-  log('copying Angular Build into the directory');
-  return src(`${paths.angular_dist}/**`)
-    .pipe(dest(`${paths.prod_build}`));
+    log('copying Angular Build into the directory');
+    return src(`${paths.angular_dist}/**`)
+        .pipe(dest(`${paths.prod_build}`));
 }
 
 function zippingProductionBuild() {
-  log('Zipping the production_build directory ');
-  return src(`${paths.prod_build}`)
-    .pipe(zip(`${paths.zipped_file_name}`));
+    log('Zipping the production_build directory ');
+    return src(`${paths.prod_build}`)
+        .pipe(zip(`${paths.zipped_file_name}`));
 }
 
 exports.default = series(
-  // clean,
-  // createProdBuildDirectory,
-  // buildAngular,
-  // copyAngularBuild,
-  zippingProductionBuild
+    clean,
+    createProdBuildDirectory,
+    buildAngular,
+    copyAngularBuild,
+    zippingProductionBuild
 );
 
