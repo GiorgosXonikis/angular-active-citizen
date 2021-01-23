f95da3ba3733f9663fd327e33e5b64f762f14425
gxo <georgios.xonikis@gmail.com>
2021-01-23 11:53:45 +0100

    use variable

--- src/environments/environment.stag.ts
+++ src/environments/environment.stag.ts
@@ -3,6 +3,8 @@ import {defaultEnv} from './environment.default';
 const stagingEnv = {
   name: 'Staging',
 
+  production: false,
+
   host: 'http://staging'
 };
 export const environment = Object.assign(defaultEnv, stagingEnv);
